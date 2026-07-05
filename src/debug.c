914041a07dc21acd23e9b6965f135081fb633189
VyaxUltima <135572786+VyaxUltima@users.noreply.github.com>
2026-07-04 22:39:46 -0500

    new song

--- src/debug.c
+++ src/debug.c
@@ -4293,6 +4293,7 @@ static void DebugAction_DestroyFollowerNPC(u8 taskId)
     X(MUS_FF5_DAWN_WARRIORS)        \
     X(MUS_3S_HUGO)                  \
     X(MUS_BW_TRAINER)               \
+    X(MUS_TURKS)                    \
     X(MUS_YAGAMI_TEAM) 
 
 #define SOUND_LIST_SE               \
